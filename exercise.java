f956afce29420692348410bafb0e6f0157745a81
Fania Hutasoit <hutasoitvania@gmail.com>
2021-10-15 13:38:39 +0700

    first commit

---
+++ exercise.java
@@ -0,0 +1,13 @@
+public class exercise {
+    public static void main(String[] args){
+        int x,y;
+        // deklarasi variabel
+
+        for (x = 1; x <= 6; x++) {
+            System.out.println(" ");
+            for (y = 1; y <= x; y++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
